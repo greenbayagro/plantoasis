7a9e977257a7a794d489e360630b108d503db0c0
darkwebplayer <amrithanadhm@gmail.com>
2023-05-18 20:40:03 +0530

    new update

--- components/home/about.vue
+++ components/home/about.vue
@@ -1,16 +1,31 @@
 <template>
-
-   <section id="about" class="relative min-w-120 my-10 p-5">
+<section id="about" class="relative">
+<div class="p-5 flex flex-wrap justify-around gap-3 py-10">
+    <div class="md:max-w-1/2">
+        <div class="font-poppins text-4xl font-bold"><span class="text-green-600">About</span> Us</div>
+        <div class="font-poppins">
+     <p class="py-2">Plant Oasis is an agro product based affiliate marketing company that is dedicated to promoting sustainable and organic agricultural practices and products . Our mission is to provide our customers with high-quality and eco-friendly products that are beneficial for both their health and the environment.</p>
+    <p class=" py-2">At Plant Oasis, we are dedicated to promoting sustainable agriculture and healthy living through our products and services. We are constantly expanding our product line and seeking out new and innovative ways to help you achieve your agricultural goals. Thank you for choosing Plant Oasis as your trusted partner in sustainable products for your land and water management,civil engineering and  agriculture based businesses.</p>
+        </div>
+    </div>
+    <div class="">
+        <img src="/img/about1.png" alt="" class="md:w-72 w-full">
+    </div>
+</div>
+     <div class="absolute -z-20 top-0 left-0">
+                                        <img src="/img/palmLT.png" alt="" class="w-48 md:w-72 opacity-30">
+                                    </div>
+</section>
+   <!-- <section id="about" class="relative min-w-120 my-10 p-5">
      <div class="relative flex justify-center items-center">
             <div class="md:container mx-auto max-w-7xl flex md:justify-center md:items-center flex-wrap gap-3">
-                <div class="md:max-w-1/2">
+                <div class="md:max-w-1/2 w-full">
                     <div class="font-poppins text-4xl font-bold"><span class="text-green-600">About</span> Us</div>
                     <div class="font-poppins">
-                        <p>Plant Oasis is an agro product based affiliate marketing company that is dedicated to promoting sustainable and organic agricultural practices and products . Our mission is to provide our customers with high-quality and eco-friendly products that are beneficial for both their health and the environment.</p>
-                        <p>At Plant Oasis, we are dedicated to promoting sustainable agriculture and healthy living through our products and services. We are constantly expanding our product line and seeking out new and innovative ways to help you achieve your agricultural goals. Thank you for choosing Plant Oasis as your trusted partner in sustainable products for your land and water management,civil engineering and  agriculture based businesses.</p>
+                       z
                     </div>
                 </div>
-                <div class="md:max-w-1/2">
+                <div class="md:max-w-1/2 w-full">
                     <img src="/img/about1.png" alt="" class="w-80">
                 </div>
             </div>
@@ -19,7 +34,7 @@
          <div class="absolute -z-20 top-0 left-0">
                                     <img src="/img/palmLT.png" alt="" class="w-48 md:w-72 opacity-40">
                                 </div>
-   </section>
+   </section>z -->
 </template>
 
 <script setup></script>
--- components/home/products.vue
+++ components/home/products.vue
@@ -1,52 +1,88 @@
 <template>
-    
-          <div class="bg-white dark:bg-gray-900">
-            <div class="md:container  py-10 max-w-7xl w-full mx-auto">
-          <div class="">
-             <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">our products</h1>
-
-                             <div class="mt-2">
-                    <span class="inline-block w-40 h-1 bg-green-700 rounded-full"></span>
-                    <span class="inline-block w-3 h-1 ml-1 bg-green-700 rounded-full"></span>
-                    <span class="inline-block w-1 h-1 ml-1 bg-green-700 rounded-full"></span>
-                </div>
-          </div>
-             <div class="flex flex-wrap max-w-7xl w-full">
-                  <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                   <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
-        <div class="w-full h-56 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style="background-image: url(https://images.unsplash.com/photo-1516223018202-9a22ff8d55f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80)"></div>
-
-        <div class="max-w-64 w-full -mt-10 overflow-hidden bg-green-700 rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
-            <h3 class="py-2 font-bold tracking-wide text-center text-white uppercase dark:text-white font-poppins">Coir pot</h3>
-            
-            <div class="flex items-center justify-end px-3 py-2 bg-white dark:bg-gray-700">
-                
-               <div class="p-3 md:text-center text-left text-md text-gray-800">
-                    Coir Pot is a biodegradable cultivation pot made of coconut fibers. It is used for horticulture, ornamental plant and nurseries, as well as for the domestic gardening market. It has an exceptionally high permeability to water, air, and roots. Coir pot is used by those looking for faster cultivation
-                </div>
+    <section id="products" class="">
+        <div class="max-w-7xl w-full p-5 mx-auto">
+            <div class="py-5 pb-10">
+                <div class="font-poppins text-4xl font-bold">Our <span class="text-green-600">Products</span></div>
             </div>
+              <swiper
+        :slidesPerView="2"
+     
+        :spaceBetween="10"
+        :centeredSlides="true"
+        :pagination="{
+            clickable: true,
+        }"
+        :breakpoints="{
+            '640': {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            '768': {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            '1024': {
+                slidesPerView: 5,
+                spaceBetween: 50,
+            },
+        }"
+       
+        class="mySwiper"
+      >
+      <swiper-slide class="w-80 h-90 relative" v-for="product,index in products" :key="index">
+        <img :src="product.image" :alt="product.name" class="object-cover w-full h-90 rounded-xl"/>
+        <div class="absolute w-full bg-gray-800 h-10 z-10 bottom-0 rounded-b-xl"><div class=" flex gap-3 justify-center items-center p-3 text-md text-white font-poppins">
+    {{ product.name }} <div class="i-ph:arrow-right-bold text-white text-xl"></div>
+        </div></div>
+    </swiper-slide>
+      
+     
+      
+   
+      </swiper>
         </div>
-    </div>
-                  </div>
-                 
-                  </div>
-                    <section class="bg-white dark:bg-gray-900">
-            <div class="px-4 py-16 mx-auto lg:flex lg:items-center lg:justify-between">
-                <h2 class="text-2xl font-semibold tracking-tight text-gray-800 xl:text-3xl dark:text-white">
-                    Download our borchure
-                </h2>
+           <div class="px-4 py-16 mx-auto lg:flex lg:items-center lg:justify-between max-w-7xl">
+                        <h2 class="text-2xl font-semibold tracking-tight font-poppins text-gray-800 xl:text-3xl dark:text-white">
+                            Download our <span class="text-green-500">borchure</span>
+                        </h2>
 
-                <div class="mt-8 lg:mt-0">
-                    <div class="flex flex-col space-y-3 sm:space-y-0 sm:flex-row sm:-mx-2">
-                        <button class="px-6 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-600 rounded-lg focus:ring focus:ring-green-300  fo sm:mx-2 hover:bg-green-500 border-none focus:bg-green-500">
-                           Download
-                        </button>
+                        <div class="mt-8 lg:mt-0">
+                            <div class="flex flex-col space-y-3 sm:space-y-0 sm:flex-row sm:-mx-2">
+                                <button class="px-6 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-600 rounded-lg focus:ring focus:ring-green-300  fo sm:mx-2 hover:bg-green-500 border-none focus:bg-green-500">
+                                   Download
+                                </button>
+                            </div>
+
+                        </div>
                     </div>
-
-                </div>
-            </div>
-        </section>
-              </div>
-            
-          </div>
+    </section>
 </template>
+<script setup>
+let products =[
+{
+    name: 'Coco Peat',
+    description: 'Coco peat is an excellent substrate for cultivation. Quality coco peat will have good water retention. Generally, coco peat or Coir pith has a very high salt content. Hence coco peat is washed with clean and natural water to lower ec levels, which is very important for root and plant growth. Washed coco peat is heat-treated, screened, and graded before being processed into coco peat products of various granularity and denseness .Coco peat contains many air pockets that increase the surface area for the root system that provides better absorption of nutrients from the soil.',
+    image: '/img/products/cocoPeat.jpeg',
+
+},
+{
+    name: 'Geo Textiles',
+    description: 'Coir geo textiles have the mechanical strength necessary to hold soil in place and prevent erosion. The netting breaks up run-off heavy rains and dissipates the energy of flowing water. Coir also promotes the growth of new vegetation by absorbing water and preventing the topsoil from drying out.',
+    image: '/img/products/GeoTextile.jpeg',
+
+},
+{
+    name: 'Coir Logs',
+    description: `Coconut coir log is a biodegradable erosion control product for hills, banks, shorelines, and other erosion-prone areas, made of 100% natural coir fibers compressed in tubular mesh netting. It is easy to install and these coir fiber logs create a natural control area that helps establish growth and control erosion. Coconut coir logs have been effectively used for erosion control in restoration projects, stabilization areas, and construction job sites.`,
+    image: '/img/products/coirlogs.jpg',
+
+},
+{
+    name: 'Garden Articles',
+    description: 'Coir Pot is a biodegradable cultivation pot made of coconut fibers. It is used for horticulture, ornamental plant and nurseries, as well as for the domestic gardening market. It has an exceptionally high permeability to water, air, and roots. Coir pot is used by those looking for faster cultivation.',
+    image: '/img/products/garden.jpeg',
+
+},
+
+]
+</script>
