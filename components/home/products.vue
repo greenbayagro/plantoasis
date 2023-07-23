dc87ae71555bdf8d41dc2aac53dab5ce493da57a
darkwebplayer <amrithanadhm@gmail.com>
2023-07-23 22:01:55 +0530

    replaced gdrive link

--- components/home/products.vue
+++ components/home/products.vue
@@ -50,7 +50,7 @@
 
                             <div class="mt-8 lg:mt-0 md:inline-block">
                                 <div class="flex flex-col space-y-3 sm:space-y-0 sm:flex-row sm:-mx-2">
-                                    <a target="_blank" href="https://drive.google.com/file/d/1-V1LEzX0UsJzXvcM1RRBgrx79mCbCaCz/view?usp=drive_link" class="px-6 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-600 rounded-lg focus:ring focus:ring-green-300  fo sm:mx-2 hover:bg-green-500 border-none focus:bg-green-500 no-underline font-poppins">
+                                    <a target="_blank" href="https://fliphtml5.com/download/download-pdf-file.php?str=QM9QWYvxkb39GRzlmJyQzNzYjMxgTPkl0av9mY" class="px-6 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-600 rounded-lg focus:ring focus:ring-green-300 text-center fo sm:mx-2 hover:bg-green-500 border-none focus:bg-green-500 no-underline font-poppins">
                                        Download
                                     </a>
                                 </div>
